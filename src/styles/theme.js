81f681c8f079e07bbfdd1ff0986352eef31f7bfb
Jesse Krensel <jdkrensel@gmail.com>
2023-03-14 22:35:26 -0400

    initial add

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+let theme = createTheme({
+    typography: { fontFamily: openSans.style.fontFamily },
+});
+theme = responsiveFontSizes(theme);
+export { theme };
