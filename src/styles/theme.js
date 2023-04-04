e66707cd4597978da7c10b29b45f2a403eda198f
Jesse Krensel <jdkrensel@gmail.com>
2023-04-04 15:28:28 -0400

    refactor: remove whitespace

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,7 @@ import { Open_Sans } from "next/font/google";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 let theme = createTheme({
-    typography: { fontFamily: openSans.style.fontFamily },
+  typography: { fontFamily: openSans.style.fontFamily },
 });
 theme = responsiveFontSizes(theme);
 export { theme };
