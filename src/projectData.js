7aa11921937afa69233fcf4a751726303c119617
Jesse Krensel <jdkrensel@gmail.com>
2023-04-04 20:04:53 -0400

    refactor: export project data to dedicated file

---
+++ src/projectData.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 0,
+    title: "Audiophile",
+    type: "Full-stack e-commerce site",
+    image: "/assets/audiophile/audiophile.webp",
+    bgColor: "#d87d4a",
+    liveDemo: "https://audiophile-pearl.vercel.app/",
+    githubRepo: "https://github.com/jeklnd/audiophile",
+  },
+  {
+    id: 1,
+    title: "Dictionary",
+    type: "Dynamic web app",
+    image: "assets/dictionary/iphone-12-pro-max--silver.svg",
+    bgColor: "#bf7cf2",
+    liveDemo: "https://dictionary-jdk.netlify.app/",
+    githubRepo: "https://github.com/jeklnd/dictionary-app",
+  },
+  {
+    id: 2,
+    title: "Space Tourism",
+    type: "4-page static website",
+    image: "assets/space-tourism/space-tourism-surface-studio.svg",
+    bgColor: "#070d19",
+    liveDemo: "https://space-tourism-jeklnd.netlify.app",
+    githubRepo: "https://github.com/jeklnd/space-tourism",
+  },
+  {
+    id: 3,
+    title: "Connect Four",
+    type: "Web-based game",
+    image: "assets/connect-four/connect-four-macbook-air.svg",
+    bgColor: "#ffce67",
+    liveDemo: "https://connect-four-game.herokuapp.com/",
+    githubRepo: "https://github.com/jeklnd/connect-four-game",
+  },
+];
