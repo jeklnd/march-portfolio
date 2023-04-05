80e8b98e016cd64eb13efd5bc8ba40f4a9da3b00
Jesse Krensel <jdkrensel@gmail.com>
2023-04-04 20:06:27 -0400

    feat: add animated loading screen
    refactor: wrap in MUI theme components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,26 @@
 import Layout from "@/components/layout/Layout";
+import { useState, useEffect } from "react";
+import LoadingScreen from "@/components/LoadingScreen";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }) {
-    return (
-        <Layout>
+  const [isLoading, setisLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setisLoading(false), 2500);
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        {!isLoading ? (
+          <Layout>
             <Component {...pageProps} />
-        </Layout>
-    );
+          </Layout>
+        ) : (
+          <LoadingScreen isLoading={isLoading} />
+        )}
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
