13f91f758d65735ce735d7d4f1a6d6338a7e685a
Jesse Krensel <jdkrensel@gmail.com>
2023-04-04 20:06:40 -0400

    refactor: import ProjectCard component and data object

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,52 +14,11 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import headshot from "public/headshot.jpg";
-import dictionary from "public/assets/dictionary/iphone-12-pro-max--silver.svg";
-import connectFour from "public/assets/connect-four/connect-four-macbook-air.svg";
-import spaceTourism from "public/assets/space-tourism/space-tourism-surface-studio.svg";
-import audiophile from "public/assets/audiophile/audiophile.webp";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import { motion } from "framer-motion";
+import ProjectCard from "@/components/ProjectCard";
+import { data } from "@/projectData";
 
 export default function Home() {
-  const data = [
-    {
-      id: 0,
-      title: "Audiophile",
-      type: "Full-stack e-commerce site",
-      image: audiophile,
-      bgColor: "#d87d4a",
-      liveDemo: "https://audiophile-pearl.vercel.app/",
-      githubRepo: "https://github.com/jeklnd/audiophile",
-    },
-    {
-      id: 1,
-      title: "Dictionary",
-      type: "Dynamic web app",
-      image: dictionary,
-      bgColor: "#bf7cf2",
-      liveDemo: "https://dictionary-jdk.netlify.app/",
-      githubRepo: "https://github.com/jeklnd/dictionary-app",
-    },
-    {
-      id: 2,
-      title: "Space Tourism",
-      type: "4-page static website",
-      image: spaceTourism,
-      bgColor: "#070d19",
-      liveDemo: "https://space-tourism-jeklnd.netlify.app",
-      githubRepo: "https://github.com/jeklnd/space-tourism",
-    },
-    {
-      id: 3,
-      title: "Connect Four",
-      type: "Web-based game",
-      image: connectFour,
-      bgColor: "#ffce67",
-      liveDemo: "https://connect-four-game.herokuapp.com/",
-      githubRepo: "https://github.com/jeklnd/connect-four-game",
-    },
-  ];
   return (
     <>
       <Head>
@@ -85,7 +44,15 @@ export default function Home() {
         />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <Container>
+
+      <Container sx={{ padding: ["0 2rem", "0 2.5rem"] }}>
+        <Box
+          sx={{
+            backgroundColor: "#000",
+            height: "40vh",
+            width: "2rem",
+          }}
+        ></Box>
         <Grid
           container
           rowGap={[2, 2]}
@@ -157,118 +124,7 @@ export default function Home() {
             </Box>
           </Grid>
           {data.map((project) => {
-            return (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                key={project.id}
-                sx={{
-                  padding: ["0 0.25rem", "0 0.375rem", "0 0.5rem"],
-                }}
-                component={motion.div}
-                initial={{ y: "2rem", opacity: 0 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{
-                  delay: project.id === 0 ? 1.5 : 0,
-                  ease: "easeIn",
-                }}
-              >
-                <Card
-                  elevation={0}
-                  sx={{
-                    borderRadius: "16px",
-                    backgroundColor: `${project.bgColor}60`,
-                    display: "flex",
-                    flexDirection: "column",
-                    position: "relative",
-                  }}
-                >
-                  <Button
-                    sx={{
-                      color: "#fff",
-                      position: "absolute",
-                      right: 0,
-                      margin: "0.25rem -0.5rem 0 0",
-                      zIndex: 102,
-                      "&:hover": {
-                        "& + a > div": { backgroundColor: "#000000" },
-                      },
-                    }}
-                    href={project.githubRepo}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <GitHubIcon variant="contained"></GitHubIcon>
-                  </Button>
-                  <a
-                    href={project.githubRepo}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    sx={{}}
-                  >
-                    <Box
-                      sx={{
-                        height: "8rem",
-                        width: "8rem",
-                        backgroundColor: "#fff",
-                        position: "absolute",
-                        right: 0,
-                        transform: "rotate(42.5deg) translate(-0.25rem, -6rem)",
-                        backgroundColor: "#00000090",
-                        color: "#ffffff90",
-                        "&:hover": {
-                          backgroundColor: "#000000",
-                          color: "#ffffff",
-                          cursor: "pointer",
-                        },
-                        zIndex: 101,
-                      }}
-                    ></Box>
-                  </a>
-
-                  <CardActionArea
-                    href={project.liveDemo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <CardHeader
-                      title={project.title}
-                      subheader={project.type}
-                      sx={{
-                        "& .MuiCardHeader-avatar": { order: 2, margin: "0" },
-                      }}
-                    ></CardHeader>
-                    <CardContent
-                      sx={{
-                        justifyContent: "center",
-                        display: "flex",
-                        height: "28rem",
-                        padding:
-                          project.image === audiophile
-                            ? "0"
-                            : "0 1rem 1rem 1rem",
-                      }}
-                    >
-                      <Box
-                        sx={{
-                          position: "relative",
-                          width: "100%",
-                        }}
-                      >
-                        <Image
-                          src={project.image}
-                          alt="project image"
-                          fill
-                          style={{ objectFit: "contain" }}
-                        ></Image>
-                      </Box>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            );
+            return <ProjectCard key={project.id} project={project} />;
           })}
           <Grid
             item
@@ -293,6 +149,9 @@ export default function Home() {
           </Grid>
         </Grid>
       </Container>
+      <Box
+        sx={{ backgroundColor: "#000", height: "1rem", width: "1rem" }}
+      ></Box>
     </>
   );
 }
