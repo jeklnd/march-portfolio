28cb3733b4dd2e0eea26d2a1b06dff98c4d276fd
Jesse Krensel <jdkrensel@gmail.com>
2023-04-06 14:35:30 -0400

    style: add ubuntu mono font

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,14 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { Open_Sans } from "next/font/google";
+import { Ubuntu_Mono } from "next/font/google";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
+const ubuntuMono = Ubuntu_Mono({ subsets: ["latin"], weight: "400" });
 let theme = createTheme({
-  typography: { fontFamily: openSans.style.fontFamily },
+  typography: {
+    fontFamily: openSans.style.fontFamily,
+    subtitle1: { fontFamily: ubuntuMono.style.fontFamily },
+  },
 });
 theme = responsiveFontSizes(theme);
 export { theme };
